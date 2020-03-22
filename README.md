a7aa30e0be480eaea7e13d657c5f644914731321
Steve Martin <steven_martin@me.com>
2020-03-22 17:52:55 +0000

    updated app code and angular news headlines config

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# angular-news-api
+# Angular News: API
+
+An API for the Angular News news [webapp](https://github.com/steven-martin/angular-news-webapp) and iOS app.
+
+## Features
+
+* The latest Angular news from the very best Tech Journalists.
+* Powered by the [Twitter-Headlines](https://www.npmjs.com/package/twitter-headlines) engine.
+* A single JSON endpoint, updated every 15 minutes.
+
+## Quick Start
+
+Download this repo locally, update the .env file values with your own twitter account values, and ... 
+
+```
+npm i
+npm run start
+```
+
+> This will start the API on local host (port:3000).
+
+## API Reference
+
+### Get Headlines
+
+To view the latest headlines (in JSON) simply visit:
+
+`http://localhost:3000/`
+
+> A cron job updates this list every 15 minutes.
+
---
+++ api.js
@@ -0,0 +1,38 @@
+
+require('dotenv').load();
+const cron = require('node-cron');
+const express = require('express');
+const TwitterHeadlines = require('twitter-headlines');
+
+const news = new TwitterHeadlines({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  headlines_file: './angular_news.headlines.json',
+});
+
+const app = express();
+news.get();
+
+app.get('/', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  res.send(news.headlines);
+});
+
+app.get('/categories', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  res.send(news.categories);
+});
+
+
+cron.schedule('0,15,30,45 * * * *', () => {
+  news.get();
+});
+
+const PORT = process.env.PORT || 3000;
+
+console.log('API running on Port ', PORT);
+app.listen(PORT);
